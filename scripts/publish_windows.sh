9e31889464b3585d06e504a153c5c50265a493f6
Tim Wanders <tim241@mailbox.org>
2020-05-12 20:15:19 +0200

    woops, forgot these

--- scripts/publish_windows.sh
+++ scripts/publish_windows.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
-publish_dir="${1:-publish}"
+publish_dir="publish"
+arch="$1"
 
 mkdir -p "$publish_dir"
 
@@ -14,8 +15,15 @@ cp build/ThirdParty/fastboot/AdbWinApi.dll \
 cp build/ThirdParty/fastboot/AdbWinUsbApi.dll \
     "$publish_dir/AdbWinUsbApi.dll"
 
+if [ "$arch" = "x86" ]
+then
+    libdir="/mingw32/bin"
+else
+    libdir="/mingw64/bin"
+fi
+
 # Bundle dependencies
-./scripts/bundle_dlls.sh "$publish_dir/" "$publish_dir/MotorolaFlash.exe" "/mingw32/bin"
+./scripts/bundle_dlls.sh "$publish_dir/" "$publish_dir/MotorolaFlash.exe" "$libdir"
 
 # Ship Qt
 windeployqt "$publish_dir/MotorolaFlash.exe" "$publish_dir"
